60dc1eda60a79cae26146b192cd89ff7d69e042f
Dominik Heidler <dheidler@gmail.com>
2013-04-19 23:16:05 +0200

    add 1-Wire temperature logging
    - use psutil insted of "free -m" to get memory info

--- README.md
+++ README.md
@@ -12,4 +12,7 @@ Installation:
 - pip install Flask-Bootstrap psutil pysqlite
 - Make the main directory writeable by webuser
 - Make the database directory writeable by webuser
+- Copy and edit sensors.json for 1-Wire logging
 - Run ./application.py (or use wsgi/fcgi)
+- Run `./chartdb init` to create sqlite tables for logging
+- Add something like `cd shackctl ; ./chartdb insert` to cron
--- chartdb.py
+++ chartdb.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python2
 
 import sys
+import os
 from pysqlite2 import dbapi2 as sqlite3
 from time import time
-import subprocess
 import psutil
+import json
 
 conn = sqlite3.connect('chart.db')
 c = conn.cursor()
@@ -26,24 +27,38 @@ elif len(sys.argv) > 1 and sys.argv[1] == 'insert':
 	timestamp = time()
 
 	# Temperature
+	sensorvals = {}
 	try:
 		cpu = int(open('/sys/class/thermal/thermal_zone0/temp').read())
-		cpu = round(float(cpu) / 1000, 1)
+		sensorvals['cpu'] = round(float(cpu) / 1000, 1)
 	except:
-		cpu = 0
-	indoor = 0
-	outdoor = 0
-	c.execute("INSERT INTO temperature VALUES (%i, %f, %f, %f)" % (timestamp, cpu, indoor, outdoor))
+		sensorvals['cpu'] = 0
+	try:
+		sensors = json.load(open('sensors.json'))
+		for sensor in ['indoor', 'outdoor']:
+			if sensors.get(sensor, False):
+				sensor_id = sensors[sensor]
+				sensor_file = os.path.join("/sys/bus/w1/devices", sensor_id, "w1_slave")
+				value = float(open(sensor_file).read().split("\n")[1].split()[-1][2:])
+				value /= 1000
+				sensorvals[sensor] = value
+			else:
+				sensorvals[sensor] = 0
+	except:
+		raise
+		sensorvals['indoor'] = 0
+		sensorvals['outdoor'] = 0
+	c.execute("INSERT INTO temperature VALUES (%i, %f, %f, %f)" %
+		(timestamp, sensorvals['cpu'], sensorvals['indoor'], sensorvals['outdoor'])
+	)
 
 	# Load
 	cpu = psutil.cpu_percent()
 	c.execute("INSERT INTO load VALUES (%i, %d)" % (timestamp, cpu))
 
 	# Memory
-	s = subprocess.Popen(['free', '-om'], stdout=subprocess.PIPE, stderr=open('/dev/null', 'w'), stdin=subprocess.PIPE)
-	sout = s.stdout.read().split('\n')
-	ram = int(filter(None, sout[1].split(' '))[2])
-	swap = int(filter(None, sout[2].split(' '))[2])
+	ram = int(psutil.used_phymem() / 1024 / 1024)
+	swap = int(psutil.used_virtmem() / 1024 / 1024)
 	c.execute("INSERT INTO memory VALUES (%i, %d, %d)" % (timestamp, ram, swap))
 
 conn.commit()
