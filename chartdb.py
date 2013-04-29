587767722e888fdb2b93f7bd178e4c8109e908a7
Dominik Heidler <dheidler@gmail.com>
2013-04-29 09:53:16 +0200

    try to catch sensor reading errors, provide debug

--- chartdb.py
+++ chartdb.py
@@ -12,20 +12,27 @@ config = json.load(open('chartdb.json'))
 conn = sqlite3.connect('database/chart.db')
 c = conn.cursor()
 
+def sql(statement):
+	if len(sys.argv) > 2 and sys.argv[2] == 'dryrun':
+		print statement
+		print "---"
+	else:
+		c.execute(statement)
+
 if len(sys.argv) > 1 and sys.argv[1] == 'init':
-	c.execute("""
+	sql("""
 		CREATE TABLE temperature
 		(timestamp INTEGER, cpu REAL, indoor REAL, outdoor REAL)
 	""")
-	c.execute("""
+	sql("""
 		CREATE TABLE load
 		(timestamp INTEGER, cpu INTEGER)
 	""")
-	c.execute("""
+	sql("""
 		CREATE TABLE memory
 		(timestamp INTEGER, ram INTEGER, swap INTEGER)
 	""")
-	c.execute("""
+	sql("""
 		CREATE TABLE power
 		(timestamp INTEGER, powersupply_12v REAL)
 	""")
@@ -39,31 +46,35 @@ elif len(sys.argv) > 1 and sys.argv[1] == 'insert':
 		sensorvals['cpu'] = round(float(cpu) / 1000, 1)
 	except:
 		sensorvals['cpu'] = 0
-	try:
-		for sensor in ['indoor', 'outdoor']:
-			if config['w1']['sensors'].get(sensor, False):
-				sensor_id = config['w1']['sensors'][sensor]
-				sensor_file = os.path.join("/sys/bus/w1/devices", sensor_id, "w1_slave")
-				value = float(open(sensor_file).read().split("\n")[1].split()[-1][2:])
-				value /= 1000
-				sensorvals[sensor] = value
-			else:
-				sensorvals[sensor] = 0
-	except:
-		sensorvals['indoor'] = 0
-		sensorvals['outdoor'] = 0
-	c.execute("INSERT INTO temperature VALUES (%i, %f, %f, %f)" %
+	for sensor in ['indoor', 'outdoor']:
+		if config['w1']['sensors'].get(sensor, False):
+			sensor_id = config['w1']['sensors'][sensor]
+			sensor_file = os.path.join("/sys/bus/w1/devices", sensor_id, "w1_slave")
+			for i in range(0, 3):
+				try:
+					content = open(sensor_file).read()
+					valid_crc = (content.split("\n")[0].split()[-1] == 'YES')
+					assert valid_crc
+					value = float(content.split("\n")[1].split()[-1][2:])
+					value /= 1000
+					sensorvals[sensor] = value
+					break
+				except:
+					sensorvals[sensor] = 0
+		else:
+			sensorvals[sensor] = 0
+	sql("INSERT INTO temperature VALUES (%i, %f, %f, %f)" %
 		(timestamp, sensorvals['cpu'], sensorvals['indoor'], sensorvals['outdoor'])
 	)
 
 	# Load
 	cpu = psutil.cpu_percent()
-	c.execute("INSERT INTO load VALUES (%i, %d)" % (timestamp, cpu))
+	sql("INSERT INTO load VALUES (%i, %d)" % (timestamp, cpu))
 
 	# Memory
 	ram = int(psutil.used_phymem() / 1024 / 1024)
 	swap = int(psutil.used_virtmem() / 1024 / 1024)
-	c.execute("INSERT INTO memory VALUES (%i, %d, %d)" % (timestamp, ram, swap))
+	sql("INSERT INTO memory VALUES (%i, %d, %d)" % (timestamp, ram, swap))
 
 	# Power
 	sensorvals = {}
@@ -97,7 +108,7 @@ elif len(sys.argv) > 1 and sys.argv[1] == 'insert':
 		else:
 			sensorvals[sensor] = 0
 	if got_data:
-		c.execute("INSERT INTO power VALUES (%i, %f)" %
+		sql("INSERT INTO power VALUES (%i, %f)" %
 			(timestamp, sensorvals['powersupply_12v'])
 		)
 
